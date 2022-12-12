8a8d113cd49e20cf24db0b246de28cc657cc2bcd
Blessing Uche <blessinguche@Blessings-MacBook-Air.local>
2022-12-12 18:06:12 +0000

    slight spacing changes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,15 @@
 const request = require("supertest");
 const db = require("../db/connection");
 const app = require("../app");
-const seed = require("../db/seeds/seed")
-const testData = require("../db/data/test-data/index")
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
 
 afterAll(() => {
   if (db.end) db.end();
 });
 beforeEach(() => {
-    return seed(testData)
-})
+  return seed(testData);
+});
 describe("GET /api/categories", () => {
   test("returns an array of category objects, with slug and description properties", () => {
     return request(app)
@@ -22,7 +22,7 @@ describe("GET /api/categories", () => {
         const { categories } = body;
         expect(categories).toBeInstanceOf(Array);
         expect(categories).toHaveLength(4);
-        
+
         categories.forEach((category) => {
           expect(category).toEqual(
             expect.objectContaining({
