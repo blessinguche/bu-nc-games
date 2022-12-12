60c2f109c1d48bc0814e773f137262e4812f564b
Blessing Uche <blessinguche@Blessings-MacBook-Air.local>
2022-12-12 18:35:48 +0000

    minor changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const { getCategories, getReviews } = require("./controllers");
+const { getCategories } = require("./controllers");
 
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
-app.get("/api/reviews", getReviews);
+
 
 app.use((err, req, res, next) => {
   if (err.msg !== undefined) {
