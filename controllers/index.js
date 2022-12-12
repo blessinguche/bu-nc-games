7b20e29e817d2bfe5e389960b14e62694065826f
Blessing Uche <blessinguche@Blessings-MacBook-Air.local>
2022-12-12 14:51:27 +0000

    blessing adds catch in controller and removes it from model

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
-const {
-    selectCategories
-} = require("../models");
+const { selectCategories } = require("../models");
 
 exports.getCategories = (req, res) => {
-    
-  selectCategories().then((categories) => {
-    res.status(200).send({ categories });
-  });
+  selectCategories()
+    .then((categories) => {
+      res.status(200).send({ categories });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- models/index.js
+++ models/index.js
@@ -3,7 +3,4 @@ const db = require("../db/connection");
 exports.selectCategories = () => {
     return db.query("SELECT * FROM categories;")
     .then((result) => result.rows)
-    .catch((err) => {
-        console.log(err)
-    });
 }
